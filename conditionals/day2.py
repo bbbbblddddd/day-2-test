5b989a5b798c33ea61d6d5c2a58f818cdd5e8a8b
Brian Dickson <bbbbblddddd@Brians-MacBook-Pro.local>
2022-11-08 11:50:36 +0000

    Adding condtionals lesson files

---
+++ conditionals/day2.py
@@ -0,0 +1,10 @@
+# SET is_it_raining = INPUT "Is it raining?"
+# IF is_it_raining is "yes"
+# THEN PRINT "You should take the car"
+
+is_it_raining = input("Is it raining?")
+
+if is_it_raining == "yes":
+    print("You should take the car")
+else: 
+    print("You should walk")
---
+++ conditionals/logical_operators.py
@@ -0,0 +1,5 @@
+brian_hungry = True
+brian_tired = True
+
+if brian_hungry and brian_tired:
+    print("Brian is hangry")
